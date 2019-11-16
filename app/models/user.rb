5b251a8b6679c19d101d1bc85954b7557bec93ae
Jason <sereda9623@gmail.com>
2019-11-16 18:59:35 +0200

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: { minimum: 3, maximum: 50 }
+
+  validates :password, presence: true, length: { minimum: 8 }
 end
