7a6867d77a63f2eb98c2f87c366b94f5c1d17e79
Jason <sereda9623@gmail.com>
2019-12-15 20:27:45 +0200

    Task position default value

--- app/validators/task_position_validator.rb
+++ app/validators/task_position_validator.rb
@@ -1,8 +1,6 @@
 class TaskPositionValidator < ActiveModel::EachValidator
   def validate_each(record, _attribute, value)
-    return check_position(record, value) unless record.new_record?
-
-    record.position = default_value(record)
+    check_position(record, value) unless record.new_record?
   end
 
   private
@@ -13,9 +11,5 @@ class TaskPositionValidator < ActiveModel::EachValidator
 
   def max_position(record)
     record.project.tasks.count
-  end
-
-  def default_value(record)
-    Task.where(project: record.project).count.next
   end
 end
