57bcf67e08ed834db4eb2a1aae8ecce5a5adab23
Jason <sereda9623@gmail.com>
2019-12-16 15:10:10 +0200

    Create session interactor

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -10,15 +10,8 @@ class Api::V1::SessionsController < ApplicationController
   param :username, String, I18n.t('docs.sessions.params.username'), required: true
   param :password, String, I18n.t('docs.sessions.params.password'), required: true
   def create
-    user = User.find_by_username(params[:username])
-    return render json: { error: I18n.t('errors.invalid_login') }, status: :unauthorized unless user&.authenticate(params[:password])
-
-    render json: {
-      header: I18n.t('auth'),
-      token: JsonWebToken.encode(user_id: user.id),
-      exp: Constants::TOKEN_EXP_DATE.from_now.strftime(Constants::DATE_TIME_FORMAT),
-      user: user
-    }, status: :ok
+    result = CreateSession.call(username: params[:username], password: params[:password])
+    render json: result.data, status: result.status
   end
 
   api :DELETE, '/session', I18n.t('docs.sessions.actions.destroy')
