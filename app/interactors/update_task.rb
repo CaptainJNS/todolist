30721e2c606da03bfa7c335e3d42d97c9bf7d0f3
Jason <sereda9623@gmail.com>
2019-12-16 16:58:52 +0200

    Task update position refactored

--- app/interactors/update_task.rb
+++ app/interactors/update_task.rb
@@ -2,25 +2,43 @@ class UpdateTask
   include Interactor
   include Errors
 
-  def call
+  before do
     return object_not_found!(:task) unless context.task
 
-    change_position(context.params[:position].to_i) if context.params[:position].present?
-    return object_invalid!(:task) unless Task.acts_as_list_no_update { context.task.update(context.params) }
+    validate_position if context.params[:position].present?
+  end
 
-    context.data = all_tasks_complete(context.task.project.tasks) ? { messages: [I18n.t('messages.all_tasks_complete')] } : {}
+  after { check_tasks_complete if context.params[:done].present? }
+
+  def call
+    return unless context.success?
+
+    change_position(context.params[:position].to_i) if context.params[:position].present?
+    return object_invalid!(:task) unless context.task.update(context.params.except(:position))
+
+    context.data = {}
   end
 
   private
 
   def change_position(position)
     context.task.insert_at(position)
-  rescue ArgumentError
   end
 
   def all_tasks_complete(tasks)
     tasks.each { |t| return false unless t.done }
 
     true
+  end
+
+  def check_tasks_complete
+    context.data = { messages: [I18n.t('messages.all_tasks_complete')] } if all_tasks_complete(context.task.project.tasks)
+  end
+
+  def validate_position
+    return if context.params[:position].to_i.in?(1..context.task.project.tasks.count)
+
+    context.task.errors.add(:base, I18n.t('errors.invalid_position'))
+    object_invalid!(:task)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,5 @@ class Task < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates_with TaskDeadlineValidator, attributes: :deadline, if: :deadline_changed?
-  validates_with TaskPositionValidator, attributes: :position, if: :position_changed?
   validates_with TaskDoneValidator, attributes: :done, if: :done_changed?
 end
